ad3abb7c871888ae300811551cf0ceed40e7c406
James Penner-Hahn <37003160+jepennerhahn@users.noreply.github.com>
2022-02-15 09:20:24 -0500

    Add files via upload

--- install_xraylib.sh
+++ install_xraylib.sh
@@ -1,4 +1,4 @@
-!/bin/.bash
+#!/bin/bash
 MINICONDA_INSTALLER_SCRIPT=Miniconda3-py37_4.10.3-Linux-x86_64.sh
 MINICONDA_PREFIX=/usr/local
 wget https://repo.continuum.io/miniconda/$MINICONDA_INSTALLER_SCRIPT
